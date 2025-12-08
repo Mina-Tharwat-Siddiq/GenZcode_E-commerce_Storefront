5366c6c9e593c2517aaed35e169b1e5e8075e18a
Mina Tharwat <minatharwat030@gmail.com>
2025-12-08 23:10:29 +0200

    Update api.js

--- GenZcode_E-commerce_Storefront-main/src/api.js
+++ GenZcode_E-commerce_Storefront-main/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:8080/api',
+    baseURL:'https://debi-api-main.vercel.app/api',
     headers: {'Content-Type': 'application/json'},
     timeout: 10000,
 })
